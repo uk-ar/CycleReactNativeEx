b3bcc4d1dd384c9ea2954cbb415a0f9b1afb2dc0
uk-ar <yuuki.ari@gmail.com>
2015-11-18 21:40:20 +0900

    Trace tutorial

--- index.android.js
+++ index.android.js
@@ -4,49 +4,72 @@
  */
 'use strict';
 
-var React = require('react-native');
+let React = require('react-native');
+let {Rx, run} = require('@cycle/core');
+let {makeReactNativeDriver} = require('@cycle/react-native');
+
 var {
   AppRegistry,
   StyleSheet,
   Text,
+  Image,
   View,
 } = React;
 
-var CycleReactNativeEx = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-});
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://resizing.flixster.com/DeLpPTAwX3O2LszOpeaMHjbzuAw=/53x77/dkpu1ddg7pbsk.cloudfront.net/movie/11/16/47/11164719_ori.jpg'
+  }}
+];
+
+function main({RN}) {
+  var movie = MOCKED_MOVIES_DATA[0];
+  return {
+    RN: RN.select('button').events('press')
+      .map(ev => +1)
+      .startWith(0)
+      .scan((x,y) => x+y)
+      .map(i =>
+           <View style={styles.container}>
+           <Image
+           source={{uri: movie.posters.thumbnail}}
+           style={styles.thumbnail}
+           />
+           <View style={styles.rightContainer}>
+           <Text style={styles.title}>{movie.title}</Text>
+           <Text style={styles.year}>{movie.year}</Text>
+           </View>
+           </View>
+          ),
+      // <Text style={styles.button} selector="button">Increment</Text>
+      // <Text>You have clicked the button {i} times.</Text>
+  };
+}
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  rightContainer: {
+    flex: 1,
   },
-  instructions: {
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
   },
 });
 
-AppRegistry.registerComponent('CycleReactNativeEx', () => CycleReactNativeEx);
+run(main, {
+  RN: makeReactNativeDriver('CycleReactNativeEx'),
+});
