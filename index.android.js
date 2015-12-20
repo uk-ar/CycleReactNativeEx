969d8c7410d05d31f7a72b7b0e5b52645727e78e
uk-ar <yuuki.ari@gmail.com>
2015-12-21 06:58:16 +0900

    Clean up

--- index.android.js
+++ index.android.js
@@ -6,7 +6,7 @@
 
 let React = require('react-native');
 let {Rx, run} = require('@cycle/core');
-let {makeReactNativeDriver, generateCycleRender} = require('@cycle/react-native')
+let {makeReactNativeDriver, generateCycleRender} = require('@cycle/react-native');
 let {makeHTTPDriver} = require('@cycle/http');
 var WebViewAndroid = require('react-native-webview-android');
 var Icon = require('react-native-vector-icons/FontAwesome');
@@ -33,148 +33,163 @@ var MOCKED_MOVIES_DATA = [
 ];
 
 var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
-function cellPress(){
-  console.log("pressed")
-}
 
-function renderMovie(movie) {
+function renderMovieCell(movie) {
   var TouchableElement = TouchableHighlight;
   if (Platform.OS === 'android') {
     TouchableElement = TouchableNativeFeedback;
   }
-  // <TouchableElement>
-  //</TouchableElement>
 
-  //https://github.com/facebook/react-native/issues/1908
-  return(<TouchableNativeFeedback selector="cell" item={movie}>
-         <View key="cell" style={styles.container}>
-         <Image
-         source={{uri: movie.posters.thumbnail}}
-         style={styles.thumbnail}
-         />
-         <View key="cell-part" style={styles.rightContainer}>
-         <Text style={styles.title}>{movie.title}</Text>
-         <Text style={styles.year}>{movie.year}</Text>
-         </View>
-         </View>
-         </TouchableNativeFeedback>)
-}
+  return(
+    <TouchableElement selector="cell" item={movie}>
+      <View key="cell" style={styles.container}>
+        <Image
+            source={{uri: movie.posters.thumbnail}}
+            style={styles.thumbnail}
+        />
+        <View key="cell-part" style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    </TouchableElement>
+  )}
 
 var dataSource = new ListView.DataSource({
   rowHasChanged: (row1, row2) => row1 !== row2,
 })
 
-var MySceneComponent = React.createClass({
-  render: function() {
-    return(
+  var MySceneComponent = React.createClass({
+    render: function() {
+      return(
         <View key="all" style={styles.rightContainer}>
-        <Text style={styles.button} selector="button">Increment</Text>
-        <ListView
-      dataSource = {dataSource.cloneWithRows(this.props.dataSource)}
-      renderRow ={generateCycleRender(renderMovie)}
-      style={styles.listView}
-        />
+          <Text style={styles.button} selector="button">Increment</Text>
+          <ListView
+              dataSource = {dataSource.cloneWithRows(this.props.dataSource)}
+              renderRow ={generateCycleRender(renderMovieCell)}
+              style={styles.listView}
+          />
         </View>
-    )
-  }
-})
+      )
+    }
+  })
 
-function main({RN,HTTP}) {
-  //var movie = MOCKED_MOVIES_DATA[0];
-  //let request$ = Rx.Observable.just(REQUEST_URL);
-  let request$ = RN.select('button').events('press')
-      .map(i => REQUEST_URL)
+  function main({RN,HTTP}) {
+    //var movie = MOCKED_MOVIES_DATA[0];
+    //let request$ = Rx.Observable.just(REQUEST_URL);
+    let request$ = RN.select('button').events('press')
+                     .map(i => REQUEST_URL);
+
+    let _navigator;
+    //onIconClicked
+    let foo$ = RN.select('back').events('iconClicked')
+                 .map(i => ToastAndroid.show("click", ToastAndroid.SHORT))
+                 .subscribe();
+
+    //let DetailView$ =
+    RN.select('cell').events('press')
       .do(i => console.log(i))
-        .subscribe();
-
-  let _navigator;
-
-  //let DetailView$ =
-  RN.select('cell').events('press')
-  //.map(i => i.currentTarget.props.item)
-      .do(i => console.log(i))
+      //.map(i => i.currentTarget.props.item)
       .map(i => i.currentTarget.props.item.posters.thumbnail)
       .do(i => ToastAndroid.show(i, ToastAndroid.SHORT))
-        .map(i => {
-          _navigator.push({
-            name: 'detail',
-            url:i
-          })
-          //return DetailView(i)
-        }).subscribe()
-  //navigator streem
-  //https://github.com/Reactive-Extensions/RxJS/blob/master/doc/gettingstarted/
-  //return value to sender?
-  // let backIntent$ = Rx.Observable.fromEvent(BackAndroid,'hardwareBackPress')
-  // backIntent$.subscribe(() => {
-  //   if (_navigator && _navigator.getCurrentRoutes().length > 1) {
-  //     _navigator.pop();
-  //     return true;
-  //   }
-  //   return false;
-  // })
-  BackAndroid.addEventListener('hardwareBackPress', () => {
-    if (_navigator && _navigator.getCurrentRoutes().length > 1) {
-      _navigator.pop();
-      return true;
-    }
-    return false;
-  });
+      .map(url => {
+        _navigator.push({
+          name: 'detail',
+          url:  url
+        })
+      }).subscribe();
+    //navigator streem
+    //https://github.com/Reactive-Extensions/RxJS/blob/master/doc/gettingstarted/
+    // return value to sender?
+    // let backIntent$ = Rx.Observable.fromEventPattern(BackAndroid,'hardwareBackPress')
+    // backIntent$.subscribe(() => {
+    /* if (_navigator && _navigator.getCurrentRoutes().length > 1) {
+       _navigator.pop();
+       return true;
+       }
+       return false; */
+    // })
 
-  var RouteMapper = function(route, navigator, component) {
-    if(_navigator === undefined){
-      _navigator=navigator;
-    }
+    //let backIntent$ = Rx.Observable.fromEvent(BackAndroid,'hardwareBackPress')
+    //http://jsdo.it/38elements/rxjs-fromeventpattern
+    /* let backIntent$ = Rx.Observable.fromEventPattern(
+       function add(h){
+       BackAndroid.addEventListener('hardwareBackPress', h)
+       },
+       function remove(h){
+       BackAndroid.removeEventListener('hardwareBackPress', h)
+       }
+       );
+       backIntent$.subscribe(() => {
+       if (_navigator && _navigator.getCurrentRoutes().length > 1) {
+       _navigator.pop();
+       return true;
+       }
+       return false;
+       }); */
+    /* BackAndroid.addEventListener('hardwareBackPress', () => {
+       if (_navigator && _navigator.getCurrentRoutes().length > 1) {
+       _navigator.pop();
+       return true;
+       }
+       return false;
+       }); */
 
-    if (route.name === 'search') {
-      return (
+    var RouteMapper = function(route, navigator, component) {
+      if(_navigator === undefined){
+        _navigator=navigator;
+      }
+      //{MOCKED_MOVIES_DATA}
+      if (route.name === 'search') {
+        return (
           <View key="scene" style={{flex: 1}}>
-          <MySceneComponent
-        key="my-scene"
-        //dataSource={i}
-        dataSource={MOCKED_MOVIES_DATA}
-          />
+            <MySceneComponent
+                key="my-scene"
+                dataSource = {route.dataSource}
+            />
           </View>
-          //<Icon name = "angle-left" size = {40} color="#900" />
-      )
-    } else if (route.name === 'detail') {
-      return(
+        )
+      } else if (route.name === 'detail') {
+        return(
           <View key="webview" style={{flex: 1}}>
-          <ToolbarAndroid
-        actions={[]}
-        navIcon={require('image!ic_arrow_back_white_24dp')}
-        //onIconClicked={navigationOperations.pop}
-        style={styles.toolbar}
-        titleColor="white"
-        //title={route.movie.title}
-          />
-        <WebViewAndroid url={route.url}
-      style={styles.containerWebView}
-        />
-        </View>
-    )
-  }
-  }
+            <ToolbarAndroid
+                actions={[]}
+                navIcon={require('image!ic_arrow_back_white_24dp')}
+                selector = "back"
+                //onIconClicked={navigationOperations.pop}
+                style={styles.toolbar}
+                titleColor="white"
+                //title={route.movie.title}
+                //title = "detail"
+            />
+            <WebViewAndroid url={route.url}
+                            style={styles.containerWebView}
+            />
+          </View>
+        )
+      }
+    }
 
-  //https://facebook.github.io/react/docs/top-level-api.html#react.cloneelement
-  //https://facebook.github.io/react-native/docs/direct-manipulation.html
-  //https://github.com/facebook/react-native/blob/master/Examples/Movies/Movies
-  let SearchView$ = HTTP.filter(res$ => res$.request === REQUEST_URL)
-      .mergeAll()
-      .map(res => JSON.parse(res.text).movies)
-      .startWith(MOCKED_MOVIES_DATA)
-      .map(i =>
-           <Navigator
-           key="nav"
-           initialRoute={{name: 'search', index: 0}}
-           renderScene={generateCycleRender(RouteMapper)}
-           />);
+    //https://facebook.github.io/react/docs/top-level-api.html#react.cloneelement
+    //https://facebook.github.io/react-native/docs/direct-manipulation.html
+    //https://github.com/facebook/react-native/blob/master/Examples/Movies/Movies
+    let SearchView$ =
+    HTTP.filter(res$ => res$.request === REQUEST_URL)
+        .mergeAll()
+        .map(res => JSON.parse(res.text).movies)
+        .startWith(MOCKED_MOVIES_DATA)
+        .map(i =>
+          <Navigator
+              key="nav"
+              initialRoute = {{name: 'search', dataSource: MOCKED_MOVIES_DATA}}
+              renderScene={generateCycleRender(RouteMapper)}
+          />);
 
-  return {
-    RN:SearchView$//.merge(DetailView$),
-    //HTTP: request$,
-  };
-}
+    return {
+      RN:SearchView$,//.merge(DetailView$),
+      HTTP: request$,
+    };
+  }
 
 var styles = StyleSheet.create({
   container: {
