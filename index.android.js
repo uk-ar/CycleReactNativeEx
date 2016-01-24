3db0972aa4a87c6a61a6c7c694e307e2668d4512
uk-ar <yuuki.ari@gmail.com>
2016-01-25 06:55:54 +0900

    Injected JS was called multiple

--- index.android.js
+++ index.android.js
@@ -36,9 +36,23 @@ var MOCKED_MOVIES_DATA = [
   {title: "はじめてのABCえほん", author: "仲田利津子/黒田昌代",
    thumbnail: "http://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/7472/9784828867472.jpg?_ex=200x200"
   },
-  {title: "はじめてのABCえほん", author: "仲田利津子/黒田昌代",
-   thumbnail: "http://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/7472/9784828867472.jpg?_ex=200x200"
-  }//size 200x200 largeImageUrl 64x64
+  {title: "ぐりとぐら(複数蔵書)", author: "中川李枝子/大村百合子",
+   thumbnail: "http://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/0825/9784834000825.jpg?_ex=200x200", isbn: "9784834000825",
+   libraryStatus: {
+     exist: true,
+     rentable: true,
+     reserveUrl: "http://api.calil.jp/reserve?id=af299d780fe86cf8b116dfda4725dc0f"
+   }
+  },
+  {title: "ぐりとぐらの1ねんかん(単一蔵書)",
+   author: "中川李枝子/山脇百合子（絵本作家）", isbn: "9784834014655",
+   libraryStatus: {
+     exist: true,
+     rentable: true,
+     reserveUrl: "https://library.city.fuchu.tokyo.jp/licsxp-opac/WOpacTifTilListToTifTilDetailAction.do?tilcod=1009710046217"},
+   thumbnail: "http://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/4655/9784834014655.jpg?_ex=200x200"
+  }
+  //size 200x200 largeImageUrl 64x64
 ];
 
 const LIBRARY_ID = "Tokyo_Fuchu"
@@ -227,9 +241,9 @@ function main({RN, HTTP}) {
               //title = "detail"
           />
           <WebView url={route.url}
-                   javaScriptEnabled={true}
                    domStorageEnabled={true}
                    startInLoadingState={true}
+                   javaScriptEnabled={true}
                    onError = {i => console.log("on err:%O", i)}
                    onLoad = {i => console.log("on load:%O", i)}
                    onLoadEnd = {i => console.log("on load end:%O", i)}
@@ -238,7 +252,8 @@ function main({RN, HTTP}) {
                    style={styles.WebViewContainer}
           />
         </CycleView>
-      )
+      ) // 'document.querySelector("[value$=\'カートに入れる\']").click()'
+        // injectedJavaScript='document.querySelector(".button").click()'
     }
   }
   /* state$.booksWithStatus$
