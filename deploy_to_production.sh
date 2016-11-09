b117fa78fc4365c3c0af79740b72ee585caf3f56
uk-ar <yuuki.ari@gmail.com>
2016-11-10 03:28:07 +0900

    Fix crash on device caused by build configuration difference

--- deploy_to_production.sh
+++ deploy_to_production.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -ex
 
+cp -f ios/CycleReactNativeEx/AppDelegate.apphub.m ios/CycleReactNativeEx/AppDelegate.m
 echo '{"appHubId":"'$appHubId'","appHubSecret":"'$appHubSecret'"}' > .apphub
 ./node_modules/.bin/apphubdeploy -t "all" -a 1.0
+git checkout -- ios/CycleReactNativeEx/AppDelegate.m
 #\n$appHubSecret'
