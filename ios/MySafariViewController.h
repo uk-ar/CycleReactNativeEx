b88d223fb8cda8c9a3fa9ef130361a806005a63c
uk-ar <yuuki.ari@gmail.com>
2016-12-01 06:51:33 +0900

    Update to RN 0.38(Enable AppGroup)

--- ios/MySafariViewController.h
+++ ios/MySafariViewController.h
@@ -6,8 +6,8 @@
 //  Copyright © 2016年 Facebook. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import <RCTBridge.h>
 
-//@interface MySafariViewController : UIViewController
 @interface MySafariViewController : NSObject <RCTBridgeModule>
 @end
--- ios/MySafariViewController.m
+++ ios/MySafariViewController.m
@@ -1,28 +1,18 @@
 #import "MySafariViewController.h"
 
-//@interface MySafariViewController ()
-
-//@end
-
 @implementation MySafariViewController
 
 // Expose this module to the React Native bridge
 RCT_EXPORT_MODULE()
-
-RCT_EXPORT_METHOD(getAppGroupPath:(NSString *) appGroupId :(RCTResponseSenderBlock)callback) {
-  NSFileManager *fileManager = [NSFileManager defaultManager];
-  NSURL *destURL = [fileManager containerURLForSecurityApplicationGroupIdentifier: appGroupId];
-  callback(@[[NSNull null], [destURL absoluteString]]); ;
-}
 
 - (NSDictionary *)constantsToExport
 {
   NSFileManager *fileManager = [NSFileManager defaultManager];
   NSURL *destURL = [[fileManager containerURLForSecurityApplicationGroupIdentifier: @"group.org.reactjs.native.example.CycleReactNativeEx"]
                     URLByAppendingPathComponent:@"test.realm"];
+  // TODO:FIX appGroup when upgrade
   return @{ @"appGroupPath":[destURL absoluteString] };
+  //return @{ @"appGroupPath":@"bar" };
 }
 
 @end
-
-
